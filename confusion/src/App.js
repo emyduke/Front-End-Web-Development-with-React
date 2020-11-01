e6bbdc3c95d95fb7f0ebf4fef8a0a224cffc99d2
emyduke <emydukeifoulimre@gmail.com>
2020-11-01 02:33:22 +0100

    Components Part 1

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 
+import Menu from './components/MenuComponent';
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +12,8 @@ function App() {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+
+        <Menu />
       </div>
   );
 }
